a67b853ed1bac4c5c111edf6ef9f555622002f2a
davidmarin <davidmarin2211@gmail.com>
2024-04-14 17:43:24 -0500

    Ajustes en App

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import './App.css';
-import { Login } from './login/components/Login';
-import { Register } from './register/components/Register';
 import { RouterLogin } from './routes/RouterLogin';
 
 const App = () => {
