76a09810444059dd311c9a857b9593faa4371678
Julio Cesar <juliocace21@gmail.com>
2024-04-16 00:41:58 -0500

    Julio Caceres: cambio de extension de los mocks de JSON a TS

---
+++ src/mocks/category.ts
@@ -0,0 +1,26 @@
+export const category = [
+  {
+    "id": 1,
+    "name": "Cirugia"
+  },
+  {
+    "id": 2,
+    "name": "Bacteriologia"
+  },
+  {
+    "id": 3,
+    "name": "Odontologia"
+  },
+  {
+    "id": 4,
+    "name": "Dermatologia"
+  },
+  {
+    "id": 5,
+    "name": "Oftalmologia"
+  },
+  {
+    "id": 6,
+    "name": "Neurologia"
+  }
+]
---
+++ src/mocks/doctors.ts
@@ -0,0 +1,67 @@
+import { Doctors } from "../types";
+
+export const Doctor:Doctors = [
+  {
+    "id": 1,
+    "gender": "female",
+    "name" : "Anna",
+    "lastname": "Whesley",
+    "category_Id": 1,
+    "specialty": "Cirugia Pediatrica",
+    "img": "../../assets/doctors/Anna-w.png",
+    "rating": 4.5
+
+  },
+  {
+    "id": 2,
+    "gender": "male",
+    "name" : "Andres",
+    "lastname": "Brand",
+    "category_Id": 2,
+    "specialty": "Bacteriologia",
+    "img": "../../assets/doctors/Andres-b.png",
+    "rating": 4.0
+  },
+  {
+    "id": 3,
+    "gender": "male",
+    "name" : "Maximo",
+    "lastname": "Smith",
+    "category_Id": 1,
+    "specialty": "Neurocirugia",
+    "img": "../../assets/doctors/Anna-w.png",
+    "rating": 4.1
+  },
+  {
+    "id": 4,
+    "gender": "female",
+    "name" : "Emma",
+    "lastname": "Villa",
+    "category_Id": 3,
+    "specialty": "Ortondocia",
+    "img": "../../assets/doctors/Anna-w.png",
+    "rating": 3.9
+  },
+  {
+    "id": 5,
+    "gender": "male",
+    "name" : "Jackson",
+    "lastname": "Avery",
+    "category_Id": 1,
+    "specialty": "Cirugia Plastica",
+    "img": "../../assets/doctors/Anna-w.png",
+    "rating": 3.9
+  },
+  {
+    "id": 6,
+    "gender": "female",
+    "name" : "Izzie",
+    "lastname": "Stevens",
+    "category_Id": 5,
+    "specialty": "Oculoplastia",
+    "img": "../../assets/doctors/Anna-w.png",
+    "rating": 4.4
+  }
+  
+
+]
