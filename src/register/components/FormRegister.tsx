fe34563f0b3e4df1cbce25ffaa580864e67738dd
davidmarin <davidmarin2211@gmail.com>
2024-04-25 21:38:33 -0500

    Ajustes en el register guardado de usuarios

---
+++ src/register/components/FormRegister.tsx
@@ -0,0 +1,139 @@
+
+import { useForm, FieldError } from 'react-hook-form';
+
+interface User {
+    name: string,
+    email: string,
+    rol: string,
+    password : string,
+    confirmedPassword: string
+}
+
+export const FormRegister = () => {
+
+  const { handleSubmit , register, watch, formState: { errors }} = useForm();
+
+  const onSubmit = handleSubmit((data) => {
+    let arrayUser = [];
+    let users:any = localStorage.getItem("users");
+    let bandera = true;
+
+    if (users != null && users != '') {
+        arrayUser = JSON.parse(users);
+
+        for (let i = 0; i < arrayUser.length; i++) {
+            if (data.email === arrayUser[i]['email']) {
+                bandera = false;
+                break;
+            }
+        }
+        arrayUser.push(data);
+    } else {
+        arrayUser.push(data);
+    }
+
+    if (bandera) {
+        localStorage.setItem('users', JSON.stringify(arrayUser));
+    }
+// console.log(arrayUser);
+  }) 
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className='fields'>
+            <label htmlFor="">Nombre</label>
+            <input 
+                type="text"
+                {...register( "name", {
+                    required: {
+                        value: true,
+                        message: "Nombre es requerido"
+                    },
+                    minLength: {
+                        value: 3,
+                        message: "Debe de tener al menos 3 caracteres."
+                    }
+                })}
+            />
+            {
+                errors.name && <span className='span_errors'>{(errors.name?.message?.toString())}</span>
+            }
+        </div>
+        <div className='fields'>
+            <label htmlFor="">Correo Electrónico</label>
+            <input  
+                type="text"
+                {...register( "email", {
+                    required: {
+                        value: true,
+                        message: "El email es requerido"
+                    },
+                    pattern: {
+                        value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                        message: "Correo no valido"
+                    }
+                })}
+            />
+            {
+                errors.email && <span className='span_errors'>{(errors.email?.message?.toString())}</span>
+            }
+        </div>
+        <div className='fields'>
+            <label htmlFor="">Rol</label>
+            <select
+                id="rol_select"
+                {...register("rol", {
+                    required: "El rol es requerido"
+                })}
+            >
+                <option value="">Elegir opcion</option>
+                <option value="1">Paciente</option>
+                <option value="2">Médico</option>
+            </select>
+            {
+                errors.rol && <span className='span_errors'>{(errors.rol?.message?.toString())}</span>
+            }
+        </div>
+        <div className='fields'>
+            <label htmlFor="">Contraseña</label>
+            <input 
+                type="password"
+                {...register( "password", {
+                    required: {
+                        value: true,
+                        message: "La contraseña es requerida"
+                    },
+                    minLength: {
+                        value: 8,
+                        message: "Debe de tener al menos 8 caracteres."
+                    }
+                })}
+            />
+            {
+                errors.password && <span className='span_errors'>{(errors.password?.message?.toString())}</span>
+            }
+        </div>
+        <div className='fields'>
+            <label htmlFor="">Confirmar Contraseña</label>
+            <input 
+                type="password"
+                {...register( "confirmedPassword", {
+                    required: {
+                        value: true,
+                        message: "La confirmación es requerida"
+                    },
+                    validate: value => value === watch('password') || "Las contraseñas no coinciden."
+                })}
+            />
+            {
+                errors.confirmedPassword && <span className='span_errors'>{(errors.confirmedPassword?.message?.toString())}</span>
+            }
+        </div>
+        <div className='button_register'>
+            <button type='submit' >REGISTRAR</button>
+        </div>
+    </form>
+    </div>
+  )
+}
--- src/register/components/Register.tsx
+++ src/register/components/Register.tsx
@@ -1,28 +1,10 @@
 
 import '../css/style.css';
 import logo from '../../assets/logo/logo.png';
-import { useState } from 'react';
+import { FormRegister } from './FormRegister';
+
 
 export const Register = () => {
-
-    const [passwordVa, setPasswordVa] = useState<String>('');
-
-    const authRegister = (e: any) => {
-        e.preventDefault();
-      
-        let data = e.target;
-      
-        const registerUser = {
-          name: data.name.value,
-          email: data.email.value,
-          rol: data.rol.value,
-          password: data.password.value,
-          confirmedPassword: data.confirmedPassword.value
-        }
-        console.log(registerUser);
-        let password = data.password.value;
-        let confirmedPassword = data.confirmedPassword.value;
-    }
 
   return (
     <div className='container_register'>
@@ -40,35 +22,7 @@ export const Register = () => {
                     <h2>REGISTRASE</h2>
                 </div>
                 <div className='content_form'>
-                    <form onSubmit={authRegister}>
-                        <div className='fields'>
-                            <label htmlFor="">Nombre</label>
-                            <input  type="text" name="name" required/>
-                        </div>
-                        <div className='fields'>
-                            <label htmlFor="">Correo Electrónico</label>
-                            <input  type="text" name="email" required/>
-                        </div>
-                        <div className='fields'>
-                            <label htmlFor="">Rol</label>
-                            <select name="rol" id="rol_select">
-                                <option value="">Elegir opcion</option>
-                                <option value="1">Paciente</option>
-                                <option value="2">Médico</option>
-                            </select>
-                        </div>
-                        <div className='fields'>
-                            <label htmlFor="">Contraseña</label>
-                            <input  type="password" name="password" required/>
-                        </div>
-                        <div className='fields'>
-                            <label htmlFor="">Confirmar Contraseña</label>
-                            <input  type="password" name="confirmedPassword" required/>
-                        </div>
-                        <div className='button_register'>
-                            <button type='submit' >REGISTRAR</button>
-                        </div>
-                    </form>
+                    <FormRegister />
                 </div>
             </div>
         </div>
